cb931c7b705a1c6db72c7e9bc75a19a8eacdde85
Nebojsa Ilić <nebojsa87ilic@gmail.com>
2021-09-21 17:16:13 +0200

    initial commit

---
+++ src/components/Watched.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
+import {MovieCard} from './MovieCard';
+import {WatchList} from './WatchList';
+
+
+export const Watched = () => {
+    const {watched} = useContext(GlobalContext);
+    return (
+        <div className="movie-page">
+        <div className="container">
+          <div className="header">
+            <h1 className="heading">Watched Movies</h1>
+
+            <span className="count-pill">
+              {watched.length} {watched.length === 1 ? 'Movie' : 'Movies'}
+          </span>
+          </div>
+  
+          {watched.length > 0 ? (
+            <div className="movie-grid">
+              {watched.map((movie) => (
+                <MovieCard  movie={movie} type="watched" />
+              ))}
+            </div>
+          ) : (
+            <h2 className="no-movies">No Movies in your List, add some !</h2>
+          )}
+        </div>
+      </div>
+    )
+};
+
+export default Watched; 
